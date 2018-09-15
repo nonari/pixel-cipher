8c439b21fe2c5419fa57e8b1ebe2cf2f97bb058d
nonari <nonari@users.noreply.github.com>
2018-09-16 00:47:35 +0200

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -2,20 +2,21 @@ from PIL import Image
 
 im = Image.open('image.jpg')
 px = im.load()
+w, h = im.size
 
 y0 = 1
 x0 = 1.0000001
-for i in range(0, 40000):
+for i in range(0, h * w):
     x = 1 - 1.4 * pow(x0, 2) + y0
     y = 0.3 * x0
-    x0 = x
-    y0 = y
-    xr = int(str(x)[4:9]) % 200
-    yr = int(str(y)[4:9]) % 200
+    x0 = float('%.14f' % (x))
+    y0 = float('%.14f' % (y))
+    xr = int(('%.11f' % (x))[4:9]) % w
+    yr = int(('%.11f' % (y))[4:9]) % h
 
-    p = px[i % 200, int(i / 200)]
+    p = px[i % w, int(i / w)]
     pr = px[xr, yr]
-    px[i % 200, int(i / 200)] = pr
+    px[i % w, int(i / w)] = pr
     px[xr, yr] = p
 
 im.save('decrypted.jpg', optimize=False, progressive=False, quality=100)
